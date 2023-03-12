4e45c4ada5b122429fec07337c9e497e2f1f2e4f
MinervaBF <126017587+MinervaBF@users.noreply.github.com>
2023-03-12 16:09:39 +0100

    Add files via upload

---
+++ programa1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+    int op1, op2, suma;
+
+    //Leer primer operando
+    cout << "Introduzca el primer operando: ";
+    cin >> op1;
+
+    //Leer segundo operando
+    cout << "Introduzca el segundo operando: ";
+    cin >> op2;
+
+    //Calcular la suma
+    suma=op1+op2;
+
+    //Mostrar el resultado
+    cout << "La suma de los números introducidos es: " << suma << endl;
+    return 0;
+}
